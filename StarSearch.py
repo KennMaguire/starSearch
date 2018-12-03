27a4f3a9886897d5bf49b296a6e4aaaee1f06a3e
Kenneth Maguire <kenneth.maguire@ucdenver.edu>
2018-12-03 16:30:47 -0700

    100% finished

--- StarSearch.py
+++ StarSearch.py
@@ -1,7 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 import csv
 import numpy as np
-
+import time
 #properName 6
 #x 17
 #y 18
@@ -31,8 +31,8 @@ with open("hygxyz.csv", "r") as xyzfile:
 
 
 #print(starDict)
-print(starDict['Sol'])
-print(starDict['Alpheratz'])
+#print(starDict['Sol'])
+#print(starDict['Alpheratz'])
 starList = []
 
 
@@ -48,9 +48,71 @@ for key,value in starDict.items():
     z2 = float(starDict[key][2])
 
     dist1 = getDist(x1,y1,z1,x2,y2,z2)
-    print(dist1)
+    #print(dist1)
     if(dist1 <= 10):
         total += 1
         starList.append((key,value))
-print(starList)
+#print(starList)
+print("\n\n\n")
 print("The total number of stars within 10 parsecs of Sol is " + str(total))
+distList = []
+
+# find the shortest distance from sol first time
+# change i to value with shortest value
+# get shortest distance for next
+i = 0
+starsTraversalOrder = []
+while starList:
+    for j in range(0, len(starList)):
+        x1 = float(starList[i][1][0])
+        y1 = float(starList[i][1][1])
+        z1 = float(starList[i][1][2])
+        if j == i:
+            pass
+        else:
+            x2 = float(starList[j][1][0])
+            y2 = float(starList[j][1][1])
+            z2 = float(starList[j][1][2])
+            dist2 = getDist(x1,y1,z1,x2,y2,z2)
+            distList.append((j,dist2))          #get all distance from the star and append to list
+
+
+    if distList:
+        nextStar = min(distList, key = lambda t: t[1])      #get the minimum distance in the list by the second value in the tuple
+        #print(distList)
+    #print(nextStar)
+    starsTraversalOrder.append((starList[i], nextStar[1]))
+    starList.remove(starList[i])
+    if i < nextStar[0]:
+        i = (nextStar[0]-1) #get the index of the next star to get distances for
+    else:
+        i = nextStar[0]
+    #print(starList)
+#    print(starsTraversalOrder)
+    distList.clear()
+
+
+#print(starsTraversalOrder)
+totalDist = 0
+print("\n")
+for i in range(0,10):
+    totalDist += starsTraversalOrder[i][1]
+    print(str(starsTraversalOrder[i][0][0]) + " -> " + str(starsTraversalOrder[i+1][0][0]) + ": Distance = " + str(round(starsTraversalOrder[i][1], 2)) + ", Total Distance = " + str(round(totalDist, 2)))
+
+print("...")
+
+for i in range(10, len(starsTraversalOrder)-11): #add to the total distance for all stars between index 10 and (size of list of Stars in Order - 10)
+    totalDist += starsTraversalOrder[i][1]
+
+for i in range(len(starsTraversalOrder)-11,len(starsTraversalOrder)-1):
+    totalDist += starsTraversalOrder[i][1]
+    print(str(starsTraversalOrder[i][0][0]) + " -> " + str(starsTraversalOrder[i+1][0][0]) + ": Distance = " + str(round(starsTraversalOrder[i][1], 2)) + ", Total Distance = " + str(round(totalDist, 2)))
+
+print("The total distance traversed is " + str(totalDist) + " parsecs.")
+
+"""
+    print(i[0])
+    print(i[1][0])
+    print(i[1][1])
+    print(i[1][2])
+"""
